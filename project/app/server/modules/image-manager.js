8176ae7cf8da44a9993da27fed6d32947ab4d3e6
unknown <bsu@LAPTOP-GVAN36IQ.fokus.fraunhofer.de>
2017-06-11 20:02:57 +0200

    User signup image issue fix

--- project/app/server/modules/image-manager.js
+++ project/app/server/modules/image-manager.js
@@ -1,17 +1,20 @@
 require('../../../globalconfig');
 
 exports.saveImage = function(req, res){
-    var imageFile = req.files.image;
-    var imageFileName = req.body.user + '_' + imageFile.name;
-    var imageSavePath = userImageUploadPath + imageFileName;
-    imageFile.mv(__dirname + imageSavePath, function(err){
-        if(err){
-            imageSavePath = '';
-            console.error(err);
-        }else{
-            console.log('Image saved as ' + imageFileName);
-            console.log('Image saved at'+ __dirname + imageSavePath);
-        }
-    });
+    var imageFileName = '';
+    if(req.files !== undefined){
+        var imageFile = req.files.image;
+        imageFileName = req.body.user + '_' + imageFile.name;
+        var imageSavePath = userImageUploadPath + imageFileName;
+        imageFile.mv(__dirname + imageSavePath, function(err){
+            if(err){
+                imageSavePath = '';
+                console.error(err);
+            }else{
+                console.log('Image saved as ' + imageFileName);
+                console.log('Image saved at '+ __dirname + imageSavePath);
+            }
+        });
+    }
     return imageFileName;
 };
