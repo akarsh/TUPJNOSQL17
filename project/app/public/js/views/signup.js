b38056e1a7eb79adb2852ac095545892f9c3c584
akarsh <sakarshkumar@gmail.com>
2017-07-09 12:40:46 +0200

    Added signup.jade
    back button link to signup.jade

--- project/app/public/js/views/signup.js
+++ project/app/public/js/views/signup.js
@@ -1,9 +1,9 @@
 
 $(document).ready(function(){
-	
+
 	var av = new AccountValidator();
 	var sc = new SignupController();
-	
+
 	$('#account-form').ajaxForm({
 		beforeSubmit : function(formData, jqForm, options){
 			return av.validateForm();
@@ -20,16 +20,17 @@ $(document).ready(function(){
 		}
 	});
 	$('#name-tf').focus();
-	
+
 // customize the account signup form //
-	
+
 	$('#account-form h2').text('Signup');
 	$('#account-form #sub1').text('Please tell us a little about yourself');
 	$('#account-form #sub2').text('Choose your username & password');
 	$('#account-form-btn1').html('Cancel');
 	$('#account-form-btn2').html('Submit');
 	$('#account-form-btn2').addClass('btn-primary');
-	
+	$('#account-form-btn3').html('Back');
+
 // setup the alert that displays when an account is successfully created //
 
 	$('.modal-alert').modal({ show:false, keyboard : false, backdrop : 'static' });
